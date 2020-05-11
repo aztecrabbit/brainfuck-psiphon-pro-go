b228abf3b0372d06eb0942c066e7dfab1c3e7140
Aztec Rabbit <ars.xda@gmail.com>
2020-05-12 03:10:21 +0700

    version 1.3.200512

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 const (
 	appName        = "Brainfuck Tunnel"
 	appVersionName = "Psiphon Pro Go"
-	appVersionCode = "1.3.200205"
+	appVersionCode = "1.3.200512"
 
 	copyrightYear   = "2020"
 	copyrightAuthor = "Aztec Rabbit"
